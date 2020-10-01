9a729feb0b0dfdf54b453eec08cb9c57cb2acae0
sandsentinel <can.elgezen@gmail.com>
2020-10-01 17:50:43 +0300

    Added half poly mul., poly addition and subtraction

--- Key Generation/poly_arithmetic.cu
+++ Key Generation/poly_arithmetic.cu
@@ -24,7 +24,32 @@ __global__ void barrett(unsigned long long a[], const unsigned long long b[], un
     a[i] = rc.low;
 }
 
-__host__ unsigned long long* full_poly_mul(unsigned long long* host_a, unsigned long long* host_b, unsigned long long* device_a, unsigned long long* device_b, unsigned N, cudaStream_t& stream1, cudaStream_t& stream2, unsigned long long q, unsigned long long mu, int bit_length, unsigned long long* psi_powers, unsigned long long* psiinv_powers)
+__global__ void poly_add(unsigned long long a[], const unsigned long long b[], unsigned long long q)
+{
+    register int i = blockIdx.x * 256 + threadIdx.x;
+
+    register unsigned long long ra = a[i] + b[i];
+
+    if (ra > (q << 2))
+        ra -= (q << 1);
+
+    a[i] = ra;
+}
+
+__global__ void poly_sub(unsigned long long a[], const unsigned long long b[], unsigned long long q)
+{
+    register int i = blockIdx.x * 256 + threadIdx.x;
+
+    register unsigned long long ra = a[i];
+    register unsigned long long rb = b[i];
+
+    if (ra < rb)
+        ra += (q << 1);
+
+    a[i] = ra;
+}
+
+__host__ unsigned long long* full_poly_mul(unsigned long long* host_a, unsigned long long* host_b, unsigned long long* device_a, unsigned long long* device_b, unsigned N, cudaStream_t& stream1, cudaStream_t& stream2, unsigned long long q, unsigned long long mu, int bit_length, unsigned long long* psi_powers)
 {
     size_t array_size = sizeof(unsigned long long) * N;
     unsigned long long* result = (unsigned long long*)malloc(array_size);
@@ -36,19 +61,31 @@ __host__ unsigned long long* full_poly_mul(unsigned long long* host_a, unsigned 
 
     barrett << <N / 256, 256, 0, stream2 >> > (device_a, device_b, q, mu, bit_length);
 
-    inverseNTT(device_a, N, stream2, q, mu, bit_length, psiinv_powers);
-
     cudaMemcpyAsync(result, device_a, array_size, cudaMemcpyDeviceToHost, stream2);
 
     return result;
 }
 
-__host__ void full_poly_mul_device(unsigned long long* device_a, unsigned long long* device_b, unsigned N, cudaStream_t& stream1, cudaStream_t& stream2, unsigned long long q, unsigned long long mu, int bit_length, unsigned long long* psi_powers, unsigned long long* psiinv_powers)
+__host__ void full_poly_mul_device(unsigned long long* device_a, unsigned long long* device_b, unsigned N, cudaStream_t& stream1, cudaStream_t& stream2, unsigned long long q, unsigned long long mu, int bit_length, unsigned long long* psi_powers)
 {
     forwardNTTdouble(device_a, device_b, N, stream1, stream2, q, mu, bit_length, psi_powers);
 
     barrett << <N / 256, 256, 0, stream2 >> > (device_a, device_b, q, mu, bit_length);
+}
 
-    inverseNTT(device_a, N, stream2, q, mu, bit_length, psiinv_powers);
+__host__ void half_poly_mul_device(unsigned long long* device_a, unsigned long long* device_b, unsigned N, cudaStream_t& stream, unsigned long long q, unsigned long long mu, int bit_length, unsigned long long* psi_powers)
+{
+    forwardNTT(device_b, N, stream, q, mu, bit_length, psi_powers);
 
+    barrett << <N / 256, 256, 0, stream >> > (device_a, device_b, q, mu, bit_length);
+}
+
+__host__ void poly_add_device(unsigned long long* device_a, const unsigned long long* device_b, unsigned N, cudaStream_t& stream, unsigned long long q)
+{
+    poly_add << <N / 256, 256, 0, stream >> > (device_a, device_b, q);
+}
+
+__host__ void poly_sub_device(unsigned long long* device_a, const unsigned long long* device_b, unsigned N, cudaStream_t& stream, unsigned long long q)
+{
+    poly_sub << <N / 256, 256, 0, stream >> > (device_a, device_b, q);
 }
--- Key Generation/poly_arithmetic.cuh
+++ Key Generation/poly_arithmetic.cuh
@@ -11,8 +11,16 @@
 
 __global__ void barrett(unsigned long long[], const unsigned long long[], unsigned long long, unsigned long long, int);
 
-unsigned long long* full_poly_mul(unsigned long long*, unsigned long long*, unsigned long long*, unsigned long long*, unsigned, cudaStream_t&, cudaStream_t&, unsigned long long, unsigned long long, int, unsigned long long*, unsigned long long*);
+__global__ void poly_add(unsigned long long[], const unsigned long long[], unsigned long long);
 
-void full_poly_mul_device(unsigned long long*, unsigned long long*, unsigned, cudaStream_t&, cudaStream_t&, unsigned long long, unsigned long long, int, unsigned long long*, unsigned long long*);
+__global__ void poly_sub(unsigned long long[], const unsigned long long[], unsigned long long);
+
+__host__ void poly_add_device(unsigned long long*, const unsigned long long*, unsigned N, cudaStream_t&, unsigned long long);
+
+__host__ void poly_sub_device(unsigned long long*, const unsigned long long*, unsigned N, cudaStream_t&, unsigned long long);
+
+unsigned long long* full_poly_mul(unsigned long long*, unsigned long long*, unsigned long long*, unsigned long long*, unsigned, cudaStream_t&, cudaStream_t&, unsigned long long, unsigned long long, int, unsigned long long*);
+
+void full_poly_mul_device(unsigned long long*, unsigned long long*, unsigned, cudaStream_t&, cudaStream_t&, unsigned long long, unsigned long long, int, unsigned long long*);
 
 #endif 
